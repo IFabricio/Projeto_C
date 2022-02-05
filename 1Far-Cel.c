e638872cc860a63d0d5402d09e2506a8649b8326
OneIsaq <i.fabricio@tutanota.com>
2022-02-04 22:16:23 -0300

    Código 1

---
+++ 1Far-Cel.c
@@ -0,0 +1,16 @@
+/* Transforma fahrenheit to celsius */
+#include<stdio.h>
+
+int main() {
+    float fah, cel;
+
+    printf("INSTRUÇÃO\n");
+    printf("Digite um numero de 0 a 100 que represente o fahrenheit para conversão em celsius\n\n");
+    
+    printf("fah = ");
+    scanf("%f", &fah);
+    printf("\nfahrenheit: %.2f\n", fah);
+
+    cel = 5.0*(fah-32)/9.0;
+    printf("celsius: %.2f\n", cel);
+}
